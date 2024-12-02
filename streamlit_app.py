bf801567297d349d13c09e72b4f455fc19eeafca
Katya Gozman <34196425+kgozman6159@users.noreply.github.com>
2024-12-01 20:27:18 -0500

    error bars ~working, update table units, labels
    weird things going on with the legend (legend changing depending on which error bars I try to select)

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,7 @@ import astropy.coordinates as coord
 
 import local_volume_database # idk what this does yet
 
+
 #st.set_page_config(layout="centered")
 # ---------------------load data---------------------- #
 @st.cache_data
@@ -40,30 +41,52 @@ def load_data():
 
     #dsph_mw['mass_HI_ul'] = (235600*dsph_mw['flux_HI_ul']*(dsph_mw['distance']/1000.)**2) # this is just the 10**x version of the mass_HI_ul column in dwarf_all
     #comb['mass_HI_ul'] = np.log10(235600*comb['flux_HI_ul']*(comb['distance']/1000.)**2)
+    for key in dwarf_all.keys():
+        if (key+'_em' in dwarf_all.keys()):
+            # dwarf_all[key] = -1*dwarf_all[key]
+            # dsph_mw[key] = -1*dsph_mw[key]
+            # dsph_m31[key] = -1*dsph_m31[key]
+            # dsph_lf[key] = -1*dsph_lf[key]
+            # dsph_lf_distant[key] = -1*dsph_lf_distant[key]
+            # gc_ambiguous[key] = -1*gc_ambiguous[key]
+            # gc_mw_new[key] = -1*gc_mw_new[key]  
+            # gc_harris[key] = -1*gc_harris[key]
+            # gc_dwarf_hosted[key] = -1*gc_dwarf_hosted[key]
+            # gc_other[key] = -1*gc_other[key]
+            # candidate[key] = -1*candidate[key]
+            # misc_host[key] = -1*misc_host[key]
+            dwarf_all[key+"_low"] = dwarf_all[key]-dwarf_all[key+'_em']
+            dwarf_all[key+"_low"] = dwarf_all[key+"_low"].fillna(0)
+        if (key+'_ep' in dwarf_all.keys()):
+            dwarf_all[key+"_high"] = dwarf_all[key]+dwarf_all[key+'_ep']
+            dwarf_all[key+"_high"] = dwarf_all[key+"_high"].fillna(0)
 
+        # for key in dwarf_all.keys():
+        #     if key.endswith('_em') or key.endswith('_ep'):
+        #         dwarf_all[key].fillna(0, inplace=True)
     return dwarf_all, dsph_mw, dsph_m31, dsph_lf, dsph_lf_distant, gc_ambiguous, gc_mw_new, gc_harris, gc_dwarf_hosted, gc_other, candidate, misc_host
 
 dwarf_all, dsph_mw, dsph_m31, dsph_lf, dsph_lf_distant, gc_ambiguous, gc_mw_new, gc_harris, gc_dwarf_hosted, gc_other, candidate, misc_host = load_data()
 
 
 # ---------------------dictionary of column labels and descriptions---------------------- #
-
+#print(dwarf_all['M_V_high'])
 table_descriptions = pd.read_csv('table_descriptions.csv', index_col='property', keep_default_na=False)
 #print(table_descriptions.iloc[0].unit)
 table_descriptions = table_descriptions.T.to_dict(index='property')
-print(table_descriptions.keys())
+# print(table_descriptions.keys())
 #st.write(table_descriptions)
 # st.write(table_descriptions['rhalf_physical']['desc'])
 valid_plot_cols = ['key', 'ra', 'dec', 'name', 'host', 'confirmed_real', 
                    'confirmed_dwarf', 'rhalf',  'position_angle', 'ellipticity', 
-                   'ellipticity_ul', 'distance_modulus', 'apparent_magnitude_v', 
-                   'vlos_systemic', 'vlos_sigma', 'vlos_sigma_ul', 'pmra',  'pmdec', 'metallicity_spectroscopic', 
-                'metallicity_spectroscopic_sigma',  'metallicity_spectroscopic_sigma_ul', 'rcore', 'rking', 'rad_sersic', 
-                'n_sersic', 'age', 'metallicity_isochrone', 'flux_HI', 'flux_HI_ul', 'metallicity_photometric', 
-                'metallicity_photometric_sigma', 'metallicity_photometric_sigma_ul', 'M_V', 'mass_stellar', 'distance',
+                   'distance_modulus', 'apparent_magnitude_v', 
+                   'vlos_systemic', 'vlos_sigma', 'pmra',  'pmdec', 'metallicity_spectroscopic', 
+                'metallicity_spectroscopic_sigma', 'rcore', 'rking', 'rad_sersic', 
+                'n_sersic', 'age', 'metallicity_isochrone', 'flux_HI', 'metallicity_photometric', 
+                'metallicity_photometric_sigma',  'M_V', 'mass_stellar', 'distance',
                 'll', 'bb', 'sg_xx', 'sg_yy', 'sg_zz', 'distance_gc', 'distance_m31', 'distance_lg', 'distance_host', 
-                'mass_HI', 'mass_HI_ul', 'metallicity', 'metallicity_type', 'velocity_gsr', 'velocity_lg', 
-                'mass_dynamical_wolf', 'mass_dynamical_wolf_ul', 'rhalf_physical', 'rhalf_sph_physical', 'surface_brightness_rhalf']
+                'mass_HI', 'metallicity', 'metallicity_type', 'velocity_gsr', 'velocity_lg', 
+                'mass_dynamical_wolf', 'rhalf_physical', 'rhalf_sph_physical', 'surface_brightness_rhalf']
 
 # ---------------------misc. functions---------------------- #
 # ------ M_V <-> L_V ------ #
@@ -77,61 +100,155 @@ def lum_inverse(x):
     m_x_sun=4.83
     return m_x_sun - (x )/0.4 + np.log10(2.)
 
-#type_x, type_y = 'linear', 'linear'
-#reverse_y, reverse_x = False, False
+
 
 
 def get_axis_specs(axis, key):
     type_axis = 'linear'
+    reverse_axis = False
+    label = table_descriptions[axis]['label']
+    if table_descriptions[axis]['dtype'] in ['float64'] and axis not in ['ra', 'dec']:
+        if not (dwarf_all[axis] <= 0).any():
+            type_axis = st.segmented_control(label + ' scale', ['linear', 'log'], default='linear', key=key)
     if table_descriptions[axis]['dtype'] in ['float64']:
-        if not (dwarf_all[axis] < 0).any():
-            type_axis = st.sidebar.pills(axis + ' scale', ['linear', 'log'], default='linear', key=key)
-
+        channel = 'quantitative'
+    elif table_descriptions[axis]['dtype'] in ['int64']:
+        channel = 'ordinal'
+    else:
+        channel = 'nominal'
     if axis in ['apparent_magnitude_v', "M_V"]:
         reverse_axis = True
-    else:
-        reverse_axis = False
     if table_descriptions[axis]['unit'] != "N/A":
-        axis_label = table_descriptions[axis]['label'] + ' (' + table_descriptions[axis]['unit'] + ')'
+        axis_label = label + ' (' + table_descriptions[axis]['unit'] + ')'
     else:
-        axis_label = table_descriptions[axis]['label']
+        axis_label = label
+    if type_axis is None:
+        type_axis = 'linear'
 
-    return type_axis, reverse_axis, axis_label
+    if axis+"_high" in dwarf_all.keys():
+        show_error = st.checkbox(label + ' error bars?')
+    else:
+        show_error = False
+
+    return type_axis, reverse_axis, axis_label, channel, show_error
+
+# ---------------------sidebar---------------------- #
+
+# with st.sidebar:
+#     with st.container(border=True, key='xcont') as xcont:
+#         left, right = st.columns([7,1], vertical_alignment="top")
+#         plot_xaxis = left.selectbox('x-axis', valid_plot_cols, index=1, format_func=lambda x: table_descriptions[x]['label'], label_visibility='visible')
+#         right.caption("---", help=table_descriptions[plot_xaxis]['desc'])
+#         type_x, reverse_x, xlabel, channel_x, show_xerr = get_axis_specs(plot_xaxis, 'xaxis')
+#     with st.container(border=True, key='ycont') as ycont:
+#         left, right = st.columns([7,1], vertical_alignment="top")
+#         plot_yaxis = left.selectbox('y-axis', valid_plot_cols, index=2, format_func=lambda x: table_descriptions[x]['label'])
+#         right.caption("---", help=table_descriptions[plot_yaxis]['desc'])
+#         type_y, reverse_y, ylabel, channel_y, show_yerr = get_axis_specs(plot_yaxis, 'yaxis')
+
+if "my_key1" not in st.session_state:
+    st.session_state.my_key1 = "ra"
+if "my_key2" not in st.session_state:
+    st.session_state.my_key2 = "dec"
+
+st.session_state.my_key1=st.session_state.my_key1
+st.session_state.my_key2=st.session_state.my_key2
+with st.sidebar:
+    with st.container(border=True, key='xcont') as xcont:
+        plot_xaxis = st.selectbox('x-axis', valid_plot_cols, index=1, key="my_key1", format_func=lambda x: table_descriptions[x]['label'], label_visibility='visible', help=f"{table_descriptions[st.session_state.my_key1]['desc']}")
+        #right.caption("---", help=table_descriptions[plot_xaxis]['desc'])
+        type_x, reverse_x, xlabel, channel_x, show_xerr = get_axis_specs(plot_xaxis, 'xaxis')
+    with st.container(border=True, key='ycont') as ycont:
+        plot_yaxis = st.selectbox('y-axis', valid_plot_cols, index=2, key="my_key2", format_func=lambda x: table_descriptions[x]['label'], help=f"{table_descriptions[st.session_state.my_key2]['desc']}")
+        #right.caption("---", help=table_descriptions[plot_yaxis]['desc'])
+        type_y, reverse_y, ylabel, channel_y, show_yerr = get_axis_specs(plot_yaxis, 'yaxis')
+# st.selectbox(
+#     "Make a selection",
+#     ["Default", "Apple", "Banana", "Carrot"],
+#     key="my_key",
+#     help=f"You've selected {st.session_state.my_key}"
+# )
 
 
-
-plot_xaxis = st.sidebar.selectbox('x-axis', valid_plot_cols)
-type_x, reverse_x, xlabel = get_axis_specs(plot_xaxis, 'xaxis')
-
-# if table_descriptions[plot_xaxis]['dtype'] in ['float64']:
-#     if not (dwarf_all[plot_xaxis] < 0).any():
-#         type_x = st.sidebar.pills('x-axis scale', ['linear', 'log'], default='linear', key='xscale')
-
-
-plot_yaxis = st.sidebar.selectbox('y-axis', valid_plot_cols)
-type_y, reverse_y, ylabel = get_axis_specs(plot_yaxis, 'yaxis')
-
-# if table_descriptions[plot_yaxis]['dtype'] in ['float64']:
-#     if not (dwarf_all[plot_yaxis] < 0).any():
-#         type_y = st.sidebar.pills('y-axis scale', ['linear', 'log'], default='linear', key='yscale')
-
-# if plot_xaxis in ['apparent_magnitude_v', "M_V"]:
-#     reverse_x = True
-# if plot_yaxis in ['apparent_magnitude_v', "M_V"]:
-#     reverse_y = True
-
-# if table_descriptions[plot_xaxis]['unit'] != "N/A":
-#     xlabel = table_descriptions[plot_xaxis]['label'] + ' (' + table_descriptions[plot_xaxis]['unit'] + ')'
-# else:
-#     xlabel = table_descriptions[plot_xaxis]['label']
-
-plot = st.altair_chart(alt.Chart(dsph_mw).mark_circle().encode(
-     x=alt.X(plot_xaxis, scale=alt.Scale(type=type_x, reverse=reverse_x), title=xlabel), 
-     y=alt.Y(plot_yaxis, scale=alt.Scale(type=type_y, reverse=reverse_y), title=table_descriptions[plot_yaxis]['label']), 
-     color='host').interactive(), use_container_width=True)
+# ---------------------filtering---------------------- #
+# filter by host
+# host = st.sidebar.multiselect('Host', dwarf_all['host'].unique())
+# if host:
+#     dwarf_all = dwarf_all[dwarf_all['host'].isin(host)]
+# # filter by confirmed dwarf
+# confirmed_dwarf = st.sidebar.multiselect('Confirmed Dwarf', dwarf_all['confirmed_dwarf'].unique())
+# if confirmed_dwarf:
+#     dwarf_all = dwarf_all[dwarf_all['confirmed_dwarf'].isin(confirmed_dwarf)]
+# # filter by confirmed real
+# confirmed_real = st.sidebar.multiselect('Confirmed Real', dwarf_all['confirmed_real'].unique())
+# if confirmed_real:
+#     dwarf_all = dwarf_all[dwarf_all['confirmed_real'].isin(confirmed_real)]
+# # filter by distance
+# distance = st.sidebar.slider('Distance (kpc)', dwarf_all['distance'].min(), dwarf_all['distance'].max(), (dwarf_all['distance'].min(), dwarf_all['distance'].max()))
+# dwarf_all = dwarf_all[(dwarf_all['distance'] >= distance[0]) & (dwarf_all['distance'] <= distance[1])]
+# # filter by apparent magnitude
+# apparent_magnitude_v = st.sidebar.slider('Apparent Magnitude V', dwarf_all['apparent_magnitude_v'].min(), dwarf_all['apparent_magnitude_v'].max(), (dwarf_all['apparent_magnitude_v'].min(), dwarf_all['apparent_magnitude_v'].max()))
+# dwarf_all = dwarf_all[(dwarf_all['apparent_magnitude_v'] >= apparent_magnitude_v[0]) & (dwarf_all['apparent_magnitude_v'] <= apparent_magnitude_v[1])]
+# # filter by vlos systemic
+# vlos_systemic = st.sidebar.slider('Vlos Systemic', dwarf_all['vlos_systemic'].min(), dwarf_all['vlos_systemic'].max(), (dwarf_all['vlos_systemic'].min(), dwarf_all['vlos_systemic'].max()))
+# dwarf_all = dwarf_all[(dwarf_all['vlos_systemic'] >= vlos_systemic[0]) & (dwarf_all['vlos_systemic'] <= vlos_systemic[1])]
 
 
-plot = st.empty()
+#-----create selections and conditions for interactivity-----#
+selection = alt.selection_point(fields=['host'], bind='legend',nearest=False,)
+
+
+# ---------------------plot---------------------- #
+
+charts_to_layer = []
+base_chart = alt.Chart(dwarf_all).mark_point(filled=True, opacity=1).encode(
+     x=alt.X(plot_xaxis, type=channel_x, scale=alt.Scale(type=type_x, reverse=reverse_x), title=xlabel), 
+     y=alt.Y(plot_yaxis, type=channel_y, scale=alt.Scale(type=type_y, reverse=reverse_y), title=ylabel),
+     color=alt.Color('host', scale=alt.Scale(scheme='tableau20'), legend=alt.Legend(title='Host')),
+     tooltip = ['name', 'host', plot_xaxis, plot_yaxis],
+     )#.add_params(selection).transform_filter(selection)
+
+charts_to_layer.append(base_chart)
+
+if show_xerr:
+    xerrorbars = base_chart.mark_errorbar(ticks=True).encode(
+        x=alt.X(plot_xaxis+"_low", type=channel_x, scale=alt.Scale(type=type_x, reverse=reverse_x), title=""),
+        y=alt.Y(plot_yaxis, type=channel_y, scale=alt.Scale(type=type_y, reverse=reverse_y), title=""),
+        x2=alt.X2(plot_xaxis+"_high", title=""),
+        #color=alt.Color('host', scale=alt.Scale(scheme='tableau20'), legend=alt.Legend(title='Host'))
+    ).transform_filter(
+        (alt.datum[plot_xaxis+"_low"] != 0) & (alt.datum[plot_xaxis+"_high"] != 0)
+    )
+    charts_to_layer.append(xerrorbars)
+
+if show_yerr:
+    yerrorbars = base_chart.mark_errorbar(ticks=True).encode(
+        x=alt.X(plot_xaxis, type=channel_x, scale=alt.Scale(type=type_x, reverse=reverse_x), title=""), 
+        y=alt.Y(plot_yaxis+"_low", type=channel_y, scale=alt.Scale(type=type_y, reverse=reverse_y), title=""),
+        y2=alt.Y2(plot_yaxis+"_high", title=""),
+        # yError=alt.YError(plot_yaxis + '_low'),
+        # yError2=alt.YError(plot_yaxis + '_high')
+        #color=alt.Color('host', scale=alt.Scale(scheme='tableau20'), legend=alt.Legend(title='Host'))
+        ).transform_filter(
+        (alt.datum[plot_yaxis+"_low"] != 0) & (alt.datum[plot_yaxis+"_high"] != 0)
+        )
+    charts_to_layer.append(yerrorbars)
+
+# chart2 = alt.Chart(dsph_m31).mark_circle().encode(
+#      x=alt.X(plot_xaxis, scale=alt.Scale(type=type_x, reverse=reverse_x), title=xlabel), 
+#      y=alt.Y(plot_yaxis, scale=alt.Scale(type=type_y, reverse=reverse_y), title=table_descriptions[plot_yaxis]['label']),
+#      color=alt.Color('host', scale=alt.Scale(scheme='tableau20'), legend=alt.Legend(title='Host'))
+#      ).interactive()
+
+
+def plot_dwarf_all():
+    st.altair_chart(alt.layer(*charts_to_layer).configure_legend(
+titleFontSize=18,
+labelFontSize=15
+).resolve_scale(color='independent').interactive(), use_container_width=True)
+
+plot_dwarf_all()
+
 
 #st.toast('Welcome to the Local Volume Database!')
 
