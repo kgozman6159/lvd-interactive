a45057e8751a1a302c6164357f66f63c5b76b1ef
Katya Gozman <34196425+kgozman6159@users.noreply.github.com>
2024-12-07 16:41:28 +0000

    add readthedocs and arXiv links

--- streamlit_app.py
+++ streamlit_app.py
@@ -931,9 +931,11 @@ with st.expander("Description of Catalogs"):
                         """)
 
 
+
     
 st.markdown("Check out the data on [![Repo](https://badgen.net/badge/icon/GitHub?icon=github&label)](https://github.com/apace7/local_volume_database)!")
-
+st.markdown("Look at the documentation on [![readthedocs](https://img.shields.io/badge/readthedocs-ffffff?logo=readthedocs&style=flat&color=ffffff&logoColor=8CA1AF)](https://local-volume-database.readthedocs.io/en/latest/index.html)")
+st.markdown("Read the paper on [![arXiv](https://img.shields.io/badge/arXiv-ffffff?logo=arxiv&style=flat&color=ffffff&logoColor=B31B1B)](https://arxiv.org/abs/2411.07424)")
 #st.altair_chart(charts_to_layer[1], use_container_width=True)
 
 
